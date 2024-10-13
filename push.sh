6bcc8a16ce153092b38ede9225370679e294ae1b
Adam Kariv <adam.kariv@gmail.com>
2024-10-13 13:25:36 +0300

    Deployment fix

--- push.sh
+++ push.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 scp reload.sh bonim:reload.sh
 scp .env.production bonim:.env.production
+scp librechat.yaml bonim:librechat.yaml
 ssh bonim /bin/bash -c "./reload.sh"
--- reload.sh
+++ reload.sh
@@ -3,5 +3,6 @@ rm main.zip && \
     wget https://github.com/whiletrue-industries/LibreChat/archive/refs/heads/main.zip && \
     unzip -o main.zip && \
     cp .env.production LibreChat-main/.env && \
+    cp librechat.yaml LibreChat-main/librechat.yaml && \
     cd LibreChat-main && \
     ./rerun.sh
