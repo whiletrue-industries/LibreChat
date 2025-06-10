31aca5cd50cb3e5c80163770126e16e0773163ae
Adam Kariv <adam.kariv@gmail.com>
2025-06-10 22:40:37 +0300

    reload

--- reload.sh
+++ reload.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-(docker pull ghcr.io/whiletrue-industries/botnim/botnim-api:latest || true) && \
-    rm main.zip && \
-    wget https://github.com/whiletrue-industries/LibreChat/archive/refs/heads/main.zip && \
-    unzip -o main.zip && \
+(docker pull ghcr.io/whiletrue-industries/botnim/botnim-api:add_query_modes || true) && \
+    rm add_query_modes.zip || true && \
+    wget https://github.com/whiletrue-industries/LibreChat/archive/refs/heads/add_query_modes.zip && \
+    unzip -o add_query_modes.zip && \
     cp .env.production LibreChat-main/.env && \
     cp botnim.yaml LibreChat-main/api/app/clients/tools/.well-known/openapi/botnim.yaml && \
     cp serviceAccountKey.json LibreChat-main/serviceAccountKey.json && \
